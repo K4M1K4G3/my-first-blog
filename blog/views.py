7c324159322d49190aeb8d383ec0cc19bd80fad0
marcos <guimaraesmarcosuft@gmail.com.br>
2016-03-15 20:10:49 -0300

    Adicionado umas views e mostrar post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # adicionado 14/03/2016
 
 def post_list(request):
-	return render(request,'blog/post_list.html',{})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request,'blog/post_list.html',{'posts':posts})
+
